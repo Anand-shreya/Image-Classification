d8eefa4780bf597d6437df3c33f6c0daf22aa499
Shreya <shreyaanand1501@gmail.com>
2024-06-16 18:35:21 +0530

    updated general scraping.py

--- Scraping/Train/General/scraping.py
+++ Scraping/Train/General/scraping.py
@@ -23,41 +23,41 @@ driver = webdriver.Chrome(service=service)
 
 
 #----------------For AI generated images------------------------
-# AI_images_folder = 'AI generated'
-# if not os.path.isdir(AI_images_folder):
-#     os.makedirs(AI_images_folder)
+AI_images_folder = 'AI generated'
+if not os.path.isdir(AI_images_folder):
+    os.makedirs(AI_images_folder)
 
 
-# img = 1
-# for page in range (1,300):
+img = 1
+for page in range (1,300):
 
-#     search_URL = "https://pixabay.com/images/search/ai/?pagi=%s" %(page)
-#     driver.get(search_URL)
+    search_URL = "https://pixabay.com/images/search/ai/?pagi=%s" %(page)
+    driver.get(search_URL)
 
-#     # print("woking on page: ", page)
-#     # print(img)
+    # print("woking on page: ", page)
+    # print(img)
 
-#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight/2);")
-#     time.sleep(10)
-#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight-50);")
-#     time.sleep(5)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight/2);")
+    time.sleep(10)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight-50);")
+    time.sleep(5)
 
-#     #---------------Scrolling all the way up-------------------
-#     driver.execute_script("window.scrollTo(0, 0);")  
-#     ImageContainers = driver.find_elements(By.XPATH, '//*[@class="container--MwyXl"]/a/img')
+    #---------------Scrolling all the way up-------------------
+    driver.execute_script("window.scrollTo(0, 0);")  
+    ImageContainers = driver.find_elements(By.XPATH, '//*[@class="container--MwyXl"]/a/img')
     
-#     for image in ImageContainers:
+    for image in ImageContainers:
 
-#         imageURL = image.get_attribute('src')
+        imageURL = image.get_attribute('src')
 
-#         if imageURL == 'https://pixabay.com/static/img/blank.gif': 
-#             continue
+        if imageURL == 'https://pixabay.com/static/img/blank.gif': 
+            continue
 
-#         try:
-#             download_image(imageURL, AI_images_folder, "AI_", img)
-#             img += 1
-#         except:
-#             print("Couldn't download an image %s, continuing downloading the next one"%(img))
+        try:
+            download_image(imageURL, AI_images_folder, "AI_", img)
+            img += 1
+        except:
+            print("Couldn't download an image %s, continuing downloading the next one"%(img))
 
 
 
@@ -114,26 +114,3 @@ while img<=10000:
     i += 1
 
 
-
-# search_URL = "https://www.freepik.com/search?format=search&last_filter=query&last_value=raw+photo&query=raw+photo#uuid=c5e6eab0-6441-4e52-9724-c7ab1cfb12cf"
-# driver.get(search_URL)
-# img_number = 368
-# for page in range(1,70):
-
-#     print("working on page ", page)
-#     print("starting with image", img_number)
-
-#     # -----Container for all the images in that page--------
-#     imageContainer = driver.find_elements(By.XPATH, '//*[@class="_1286nb19f _1286nb11p9"]/a') 
-
-#     for image in imageContainer:
-#         imageURL = image.get_attribute('href')
-#         try:
-#             download_image(imageURL, Real_images_folder, "Real_", img_number)
-#             img_number += 1
-#         except:
-#             print("Couldn't download an image %s, continuing downloading the next one"%(img_number))
-
-#     # -------- XPath of next button: //*[@class="pagination__next button floatl pd-y-none-i"] -----------
-#     nxt_Button = driver.find_element(By.XPATH, ' //*[@id="__next"]/div[4]/div[3]/div/div[4]/div[1]/a[2]') 
-#     nxt_Button.click()   
